46a3eb4eecc2de8c56d713893dd383bae599cad1
Diego Procopio <diegprocopio98@gmail.com>
2022-06-30 20:53:10 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export function Header() {
+    return (
+        <header className="w-full text-center bg-blue-700">
+            <ul 
+                className="flex justify-center gap-2 items-center py-8 text-cyan-50 decoration-current"
+            >
+                <li>
+                    HOME
+                </li>
+                <li>
+                    TECNOLOGIAS
+                </li>
+                <li>
+                    MEUS PROJETOS
+                </li>
+            </ul>
+        </header>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Poppins, sans-serif",
+      },
+      colors: {
+        "blue-700": "#28314E",
+      },
+    },
+  },
+  plugins: [],
+};
